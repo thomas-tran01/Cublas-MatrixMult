fae77af35d2696548b1a1ecf81db477e1d0ecc59
Thomas Tran <tran0@seas.upenn.edu>
2024-08-15 16:33:42 -0500

    mat mul implementation. need error checks

--- matrix_mult.cu
+++ matrix_mult.cu
@@ -7,7 +7,7 @@ void populateMatrix(float* h_matrix, int row, int col)
 {
     for(int i = 0; i < row * col; i++)
     {
-        h_matrix[i] = (rand()) % 100;
+        h_matrix[i] = (rand()) % 10;
     }
 }
 
@@ -22,9 +22,34 @@ void printMatrix(const float* matrix, int row, int col) {
 }
 
 
-int matrixMult(float* h_matrixA, float* h_matrixB, float *h_matrixC, int m, int n, int k)
+void matrixMult(float* h_matrixA, float* h_matrixB, float *h_matrixC, int m, int n, int k)
 {
-    return 0;
+    float* d_matrixA;
+    float* d_matrixB;
+    float* d_matrixC;
+    cublasHandle_t cublasHandle;
+    float alpha = 1.0f;
+    float beta = 0.0f;
+
+    cublasCreate(&cublasHandle);
+    int matrixASize = m * k * sizeof(float);
+    int matrixBSize = n * k * sizeof(float);
+    int matrixCSize = m * n * sizeof(float);
+    cudaMalloc(&d_matrixA, matrixASize);
+    cudaMalloc(&d_matrixB, matrixBSize);
+    cudaMalloc(&d_matrixC, matrixCSize);
+
+    cudaMemcpy(d_matrixA, h_matrixA, matrixASize, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matrixB, h_matrixB, matrixBSize, cudaMemcpyHostToDevice);
+
+    cublasSgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, d_matrixB, n, d_matrixA, k, &beta, d_matrixC, n);
+    
+    cudaMemcpy(h_matrixC, d_matrixC, matrixCSize, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_matrixA);
+    cudaFree(d_matrixB);
+    cudaFree(d_matrixC);
+    cublasDestroy(cublasHandle);
 }
 
 
@@ -34,7 +59,7 @@ int main()
     int col = 3;
     float* matrixA = new float[row*col];
     float* matrixB = new float[row*col];
-    float* matrixC;
+    float* matrixC = new float[row*col];
 
 
     populateMatrix(matrixA, row, col);
@@ -45,5 +70,7 @@ int main()
     std::cout<< "MATRIX B" << std::endl;
     printMatrix(matrixB, row, col);
 
-
+    matrixMult(matrixA, matrixB, matrixC, row, col, col);
+    std::cout<< "MATRIX C" << std::endl;
+    printMatrix(matrixC, row, col);
 }
